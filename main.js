bb7354051af730e661ef372af804213a33b3c601
CoderDoder <91969955+CoderDoder@users.noreply.github.com>
2023-06-10 19:45:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+var song1="";
+var song2="";
+
 function draw(){
     image(video,0,0,550,400);
 }
@@ -10,5 +13,11 @@ function setup(){
 }
 
 function preload(){
+    song1=loadSound("Dancin.mp3");
+    song2=loadSound("WorthNothing.mp3");
 
+}
+
+function play(){
+    song1.play()
 }
